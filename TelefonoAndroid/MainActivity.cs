31112114e4e262b014d9ab68c49e877d821b3ad5
Infraestructura <Infraestructura@iMac-de-Alumno-7.local>
2016-11-25 17:01:48 -0600

    first commit

--- TelefonoAndroid/MainActivity.cs
+++ TelefonoAndroid/MainActivity.cs
@@ -1,13 +1,14 @@
-﻿using Android.App;
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using Android.Content;
 
-namespace TelefonoAndroid
+namespace TelefonoAndriod
 {
-	[Activity(Label = "TelefonoAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "TelefonoAndriod", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -18,10 +19,45 @@ namespace TelefonoAndroid
 
 			// Get our button from the layout resource,
 			// and attach an event to it
+			EditText etNumero = FindViewById<EditText>(Resource.Id.txtNumero);
+			Button btnTraducir = FindViewById<Button>(Resource.Id.btnTraducir);
+			Button btnLlamar = FindViewById<Button>(Resource.Id.btnLlamar);
 
+			btnLlamar.Enabled = false;
 
+			string numeroTraducido = string.Empty;
 
+			btnTraducir.Click += (object sender, EventArgs e) =>
+			{
+				numeroTraducido = Traductor.TraduceNumeroACadena(etNumero.Text);
+				if (String.IsNullOrWhiteSpace(numeroTraducido))
+				{
+					btnLlamar.Text = "Llamar";
+					btnLlamar.Enabled = false;
+				}
+				else {
+					btnLlamar.Text = "Llamar " + numeroTraducido;
+					btnLlamar.Enabled = true;
+				}
+			};
 
+			btnLlamar.Click += (object sender, EventArgs e) =>
+			{
+				var ventanaLlamar = new AlertDialog.Builder(this);
+				ventanaLlamar.SetMessage("Llamar a " + numeroTraducido +"?");
+				ventanaLlamar.SetNeutralButton("Llamar", delegate {
+					var llamadaIntent = new Intent(Intent.ActionCall);
+					llamadaIntent.SetDat(Android.Net.Uri.Parse("tel: " + numeroTraducido));
+					StartActivity(llamadaIntent);
+
+				});
+
+				ventanaLlamar.SetNegativeButton("Cancelar", delegate { });
+				ventanaLlamar.Show();
+
+					             
+
+			};
 		}
 	}
 }
--- TelefonoAndroid/Traductor.cs
+++ TelefonoAndroid/Traductor.cs
@@ -1,10 +1,83 @@
-﻿using System;
-namespace TelefonoAndroid
+using System;
+using System.Text;
+
+namespace TelefonoAndriod
 {
-	public class Traductor
+	public static class Traductor
 	{
-		public Traductor()
+		public static string TraduceNumeroACadena( string num )
 		{
+			if (string.IsNullOrWhiteSpace(num))
+			{
+				return "";
+			}
+			else {
+				num = num.ToUpperInvariant();
+			}
+
+			var nuevoNumero = new StringBuilder();
+
+			foreach ( var c in num )
+			{
+				if (" -0123456789".Contains(c))
+				{
+					nuevoNumero.Append(c);
+				}
+				else {
+					var resultado = TraduceNumeroACadena( c );
+					if (resultado != null)
+					{
+						nuevoNumero.Append( resultado );
+					}
+				}
+			}
+
+			return nuevoNumero.ToString();
+		}
+
+		static bool Contains(this string keyString, char c)
+		{
+			return keyString.IndexOf(c) >= 0;
+		}
+
+		static int? TraduceNumeroACadena(char c)
+		{
+			if ("ABC".Contains(c))
+			{
+				return 2;
+			}
+			else if ("DEF".Contains(c))
+			{
+				return 3;
+			}
+			else if ("GHI".Contains(c))
+			{
+				return 4;
+			}
+			else if ("JKL".Contains(c))
+			{
+				return 5;
+			}
+			else if ("MNO".Contains(c))
+			{
+				return 6;
+			}
+			else if ("PQRS".Contains(c))
+			{
+				return 7;
+			}
+			else if ("TUV".Contains(c))
+			{
+				return 8;
+			}
+			else if ("WXYZ".Contains(c))
+			{
+				return 9;
+			}
+			else
+			{
+				return null;
+			}
 		}
 	}
 }
